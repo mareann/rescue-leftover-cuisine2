36f348966148911f0f815191115a9d39b981dc4e
Bryan Vanderhoof <bvanderhoof@heroku.com>
2019-03-07 19:17:12 -0600

    Add active storage and image model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,7 @@
 class Recipe < ApplicationRecord
   include PgSearch
   pg_search_scope :search_by_keyword, :against => [:title, :ingredients]
+
+  has_many :images, dependent: :destroy, -> { order(position: :asc) }
+  scope :with_attached_images, -> { includes(:images).merge(Image.with_attached_file) }
 end
